ba424ba6c7bdc9bad38307bf48a43fd0d1968878
poseidon9 <poseidonthenine@gmail.comm>
2018-06-21 10:01:15 +0800

    no

--- SylDesk/SylDesk/Grafica.Designer.cs
+++ SylDesk/SylDesk/Grafica.Designer.cs
@@ -29,15 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Grafica));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.label1 = new System.Windows.Forms.Label();
             this.button17 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -63,62 +67,96 @@
             this.button17.UseVisualStyleBackColor = true;
             this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(63, 71);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(84, 17);
-            this.checkBox1.TabIndex = 76;
-            this.checkBox1.Text = "Maria Series";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(174, 71);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(93, 17);
-            this.checkBox2.TabIndex = 77;
-            this.checkBox2.Text = "Charles Series";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(276, 71);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(81, 17);
-            this.checkBox3.TabIndex = 78;
-            this.checkBox3.Text = "John Series";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
             this.chart1.Location = new System.Drawing.Point(19, 94);
             this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(1052, 417);
+            this.chart1.Size = new System.Drawing.Size(545, 417);
             this.chart1.TabIndex = 82;
             this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 51);
+            this.button1.TabIndex = 83;
+            this.button1.Text = "Categoria de altura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 51);
+            this.button2.TabIndex = 84;
+            this.button2.Text = "Categoria Diametro";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 51);
+            this.button3.TabIndex = 85;
+            this.button3.Text = "Mayor numero de Individuos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 51);
+            this.button4.TabIndex = 86;
+            this.button4.Text = "Area Basal";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(580, 37);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 51);
+            this.button5.TabIndex = 87;
+            this.button5.Text = "Volumen";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(590, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(478, 417);
+            this.dataGridView1.TabIndex = 88;
             // 
             // Grafica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.label1);
             this.Name = "Grafica";
             this.Size = new System.Drawing.Size(1096, 528);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,9 +166,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- SylDesk/SylDesk/Grafica.cs
+++ SylDesk/SylDesk/Grafica.cs
@@ -28,6 +28,7 @@ namespace SylDesk
             //series1 = chart1.Series[0];
 
         }
+
         /*private void Grafica_Load(object sender, EventArgs e)
         {
            
@@ -37,13 +38,15 @@ namespace SylDesk
         {
             Empty();
             this.proyecto_id = proyecto_id;
-            get_volumen();
             //comboBoxSitios_Populate();
         }
 
         public void Empty()
         {
             chart1.Series.Clear();
+            chart1.Titles.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
             //chartPie.Series.Clear();
         }
 
@@ -54,24 +57,319 @@ namespace SylDesk
             form1.formRegistro2ToFront(proyecto_id);
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {            
-            if(checkBox1.Checked)
-            {
+        private void get_cat() //individuos por categorias de altura
+        {
+            Empty();
+            chart1.Titles.Add("Detalles de Categorias de AlTura");   //titulo de la Grafica
+            dataGridView1.Columns.Add("cat", "categoria de altura");
+            dataGridView1.Columns.Add("conteo", "conteo");
 
-                chart1.Series[0].Enabled = true;
-            }
-            else
-            {
+            double rango_cat = 0;
 
-                chart1.Series[0].Enabled = false;
+            cmd = SqlConnector.getConnection(cmd);
+
+            string sqlQueryString = "SELECT Min(alturatotal) as minimo " +
+                " from individuos where proyecto_id = @proyecto_id AND area = 500 AND alturatotal > 0";
+            cmd.CommandText = sqlQueryString;
+            cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+            var results = cmd.ExecuteReader();
+            results.Read();
+            double min = Convert.ToDouble(results[0]);
+           results.Close();
+            results.Dispose();
+
+
+            cmd = SqlConnector.getConnection(cmd);
+
+            sqlQueryString = "SELECT Max(alturatotal) as maximo " +
+                " from individuos where proyecto_id = @proyecto_id AND area = 500 AND alturatotal > 0";
+            cmd.CommandText = sqlQueryString;
+            cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+            results = cmd.ExecuteReader();
+            results.Read();
+            double max = Convert.ToDouble(results[0]);
+           
+            results.Close();
+            results.Dispose();
+
+            if (max <= 15)
+            {
+                rango_cat = 2;
             }
+            else if (max <= 20)
+            {
+                rango_cat = 2.5;
+            }
+            else if (max <= 25)
+            {
+                rango_cat = 5;
+            }
+            else if (max <= 30)
+            {
+                rango_cat = 7.5;
+            }
+            else if (max <= 35)
+            {
+                rango_cat = 10;
+            }
+            else if (max <= 40)
+            {
+                rango_cat = 12.5;
+            }
+
+            double rango_cat2 = rango_cat / 2;
+            double current_rango = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                cmd = SqlConnector.getConnection(cmd);
+
+                double lower_point = current_rango - rango_cat2;
+                double upper_point = current_rango + rango_cat2;
+                if (i == 0)
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND alturatotal < " + upper_point;
+                }
+                else if (i == 4)
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND alturatotal > " + lower_point;
+                }
+                else
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND alturatotal > " + lower_point + " AND alturatotal < " + upper_point;
+                }
+
+                cmd.CommandText = sqlQueryString;
+                cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+                results = cmd.ExecuteReader();
+                results.Read();
+                chart1.Series.Add(new kawaii_lolis.Series("" + current_rango));
+                chart1.Series[i].Points.AddXY("" + current_rango, results[0]);
+                chart1.Series[i].ToolTip = "CAT: #VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+                dataGridView1.Rows.Add(current_rango, results[0]);
+
+                results.Close();
+                results.Dispose();
+
+                current_rango += rango_cat;
+            }
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+            /*
+            chart1.Series.Add(new kawaii_lolis.Series("Min"));
+            chart1.Series[chart1.Series.Count - 1].Points.AddXY("" + min, min);
+            chart1.Series[chart1.Series.Count - 1].ToolTip = "#VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+            chart1.Series.Add(new kawaii_lolis.Series("Max"));
+            chart1.Series[chart1.Series.Count - 1].Points.AddXY("" + max, max);
+            chart1.Series[chart1.Series.Count - 1].ToolTip = "#VALX\nConteo: #VALY ";          //Tooltips para cada barra
+            */
+        }
+
+        private void get_cad() //individuos categoria diametricas
+        {
+            Empty();
+            chart1.Titles.Add("Detalles de Categorias Diametricas");   //titulo de la Grafica
+            dataGridView1.Columns.Add("cad", "categoria de diametro");
+            dataGridView1.Columns.Add("conteo", "conteo");
+
+            double rango_cad = 0;
+
+            cmd = SqlConnector.getConnection(cmd);
+
+            string sqlQueryString = "SELECT Min(diametro) as minimo " +
+                " from individuos where proyecto_id = @proyecto_id AND area = 500 AND diametro > 0";
+            cmd.CommandText = sqlQueryString;
+            cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+            var results = cmd.ExecuteReader();
+            results.Read();
+            double min = Convert.ToDouble(results[0]);
+            results.Close();
+            results.Dispose();
+
+
+            cmd = SqlConnector.getConnection(cmd);
+
+            sqlQueryString = "SELECT Max(diametro) as maximo " +
+                " from individuos where proyecto_id = @proyecto_id AND area = 500 AND diametro > 0";
+            cmd.CommandText = sqlQueryString;
+            cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+            results = cmd.ExecuteReader();
+            results.Read();
+            double max = Convert.ToDouble(results[0]);
+
+            results.Close();
+            results.Dispose();
+
+            if (max <= 20)
+            {
+                rango_cad = 2;
+            }
+            else if (max <= 25)
+            {
+                rango_cad = 2.5;
+            }
+            else if (max <= 30)
+            {
+                rango_cad = 5;
+            }
+            else if (max <= 35)
+            {
+                rango_cad = 7.5;
+            }
+            else if (max <= 40)
+            {
+                rango_cad = 10;
+            }
+            else if (max <= 45)
+            {
+                rango_cad = 12.5;
+            }
+            else if (max <= 50)
+            {
+                rango_cad = 15;
+            }
+            else if (max <= 55)
+            {
+                rango_cad = 17.5;
+            }
+            else if (max <= 60)
+            {
+                rango_cad = 20;
+            }
+
+            double rango_cad2 = rango_cad / 2;
+            double current_rango = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                cmd = SqlConnector.getConnection(cmd);
+
+                double lower_point = current_rango - rango_cad2;
+                double upper_point = current_rango + rango_cad2;
+                if (i == 0)
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND diametro < " + upper_point;
+                }
+                else if (i == 4)
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND diametro > " + lower_point;
+                }
+                else
+                {
+                    sqlQueryString = "SELECT Count(*) as conteo " +
+                        " from individuos where proyecto_id = @proyecto_id AND area = 500 AND diametro >= " + lower_point + " AND diametro < " + upper_point;
+                }
+
+                cmd.CommandText = sqlQueryString;
+                cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+                results = cmd.ExecuteReader();
+                results.Read();
+                chart1.Series.Add(new kawaii_lolis.Series("" + current_rango));
+                chart1.Series[i].Points.AddXY("" + current_rango, results[0]);
+                chart1.Series[i].ToolTip = "CaD #VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+                dataGridView1.Rows.Add(current_rango, results[0]);
+
+                results.Close();
+                results.Dispose();
+
+                current_rango += rango_cad;
+            }
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            /*
+            chart1.Series.Add(new kawaii_lolis.Series("Min"));
+            chart1.Series[chart1.Series.Count - 1].Points.AddXY("" + min, min);
+            chart1.Series[chart1.Series.Count - 1].ToolTip = "#VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+            chart1.Series.Add(new kawaii_lolis.Series("Max"));
+            chart1.Series[chart1.Series.Count - 1].Points.AddXY("" + max, max);
+            chart1.Series[chart1.Series.Count - 1].ToolTip = "#VALX\nConteo: #VALY ";          //Tooltips para cada barra
+            */
+        }
+
+        private void get_numero_individuos() // Mayor numero de individuos en el predio
+        {
+            Empty();
+            chart1.Titles.Add("Detalles de Conteo");   //titulo de la Grafica
+            dataGridView1.Columns.Add("numero_individuos", "numero de individuos");
+            dataGridView1.Columns.Add("conteo", "conteo");
+
+            cmd = SqlConnector.getConnection(cmd);  
+
+            string sqlQueryString = "SELECT nombrecientifico, Count(nombrecientifico) as conteo " +
+                " from individuos where proyecto_id = @proyecto_id AND area = 500 AND bifurcados = 0 Group By nombrecientifico ORDER BY conteo DESC";
+            cmd.CommandText = sqlQueryString;
+            cmd.Parameters.AddWithValue("@proyecto_id", proyecto_id);
+
+            var results = cmd.ExecuteReader();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (results.Read())
+                {
+                    List<Object> lista_individuos = new List<Object>();
+                    lista_individuos.Add(results[0]);
+                    lista_individuos.Add(results[1]);
+                    //lista_individuos.Add(results[2]);
+
+                    chart1.Series.Add(new kawaii_lolis.Series("" + results[0]));
+                    chart1.Series[i].Points.AddXY("" + results[0], results[1]);
+                    
+                    dataGridView1.Rows.Add(results[0], results[1]);
+
+                    //chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+
+                    chart1.Series[i].ToolTip = "#VALX\nConteo: #VALY ";          //Tooltips para cada barra
+                }
+                else
+                {
+                    break;
+                }
+            }
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+            results.Close();
+            results.Dispose();
+        }
+
+        private void get_area_basal() // area basal por especie (m^2 / ha)
+        {
+            Empty();
+            chart1.Titles.Add("Detalles de Area Basal");   //titulo de la Grafica
+            dataGridView1.Columns.Add("especie", "especie de altura");
+            dataGridView1.Columns.Add("suma_ab", "suma de ab");
+            dataGridView1.Columns.Add("ab_ha", "AB (m2/ha)");
+            dataGridView1.Columns.Add("ab_ha2", "AB (m2/2.812 ha)");
+
+
+
+
+
+
+
+            chart1.ChartAreas[0].RecalculateAxesScale();
 
         }
 
-
         private void get_volumen()
         {
+            Empty();
+
+            chart1.Titles.Add("Detalles de Volumen");   //titulo de la Grafica
+            dataGridView1.Columns.Add("especie", "especie");
+            dataGridView1.Columns.Add("conteo", "conteo");
+
             cmd = SqlConnector.getConnection(cmd);
 
             string sqlQueryString = "SELECT nombrecientifico, Sum(volumenvv) as volumen " +
@@ -82,7 +380,6 @@ namespace SylDesk
             var results = cmd.ExecuteReader();
 
 
-            chart1.Titles.Add("Detalles de Volumen");   //titulo de la Grafica
             
 
             for (int i = 0; i < 5; i++)
@@ -94,15 +391,12 @@ namespace SylDesk
                     lista_individuos.Add(results[1]);
                     //lista_individuos.Add(results[2]);
 
-                    
-
-                   
                     chart1.Series.Add(new kawaii_lolis.Series("" + results[0]));
                     chart1.Series[i].Points.AddXY("" + results[0], results[1]);
 
-
+                    dataGridView1.Rows.Add(results[0], results[1]);
                     //chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-                    
+
                     chart1.Series[i].ToolTip = "#VALX\nVolumen: #VALY ";          //Tooltips para cada barra
                 }
                 else
@@ -110,6 +404,7 @@ namespace SylDesk
                     break;
                 }
             }
+            chart1.ChartAreas[0].RecalculateAxesScale();
 
             results.Close();
             results.Dispose();
@@ -123,6 +418,31 @@ namespace SylDesk
             //MessageBoxButton button = MessageBoxButton.OK;
             //MessageBoxImage icon = MessageBoxImage.Error;
             MessageBox.Show(messageBoxText, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            get_cat();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            get_cad();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            get_numero_individuos();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            get_area_basal();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            get_volumen();
         }
     }
 }
