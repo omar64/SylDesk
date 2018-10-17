fd8db2afa5347037e7d7ec96d7b4fc8518ed8d35
omarER <110300113@ucaribe.edu.mx>
2018-10-17 04:04:15 -0500

    miercoles 17 2

--- SylDesk/SylDesk/Form1.cs
+++ SylDesk/SylDesk/Form1.cs
@@ -16,7 +16,7 @@ namespace SylDesk
         public Form1()
         {
             InitializeComponent();
-            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            
 
             this.formRegistro1.setForm(this);
             this.formRegistro2.setForm(this);
@@ -33,29 +33,7 @@ namespace SylDesk
             formInicialToFront();
         }
 
-        //Resize
-        private const int cGrip = 16;
-        private const int cCaption = 32;
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == 0x84)
-            {
-                Point pos = new Point(m.LParam.ToInt32());
-                pos = this.PointToClient(pos);
-                if (pos.Y < cCaption)
-                {
-                    m.Result = (IntPtr)2;
-                    return;
-                }
-                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
-                {
-                    m.Result = (IntPtr)17;
-                    return;
-                }
-            }
-            base.WndProc(ref m);
-        }
+        
 
 
 
