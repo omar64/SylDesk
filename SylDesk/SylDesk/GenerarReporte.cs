899600b27278f5116d43037a2f56321bcba2fe87
poseidon9 <poseidonthenine@gmail.comm>
2018-12-11 03:30:48 +0800

    adskw21e3

--- SylDesk/SylDesk/GenerarReporte.cs
+++ SylDesk/SylDesk/GenerarReporte.cs
@@ -29,120 +29,243 @@ namespace SylDesk
         public void Initialize(Proyecto proyecto)
         {
             this.proyecto = proyecto;
+
+            ///
+            ///Tabla y Grafica de Categoria de altura(cat)
+            ///
+            List<Cat> list_cat = getCats();  //get list of students
+            //reportViewer1.LocalReport.DataSources.Clear(); //clear report
+            //reportViewer1.LocalReport.ReportEmbeddedResource = "Syldesk.ReporteFormato.rdlc"; // bind reportviewer with .rdlc
+            Microsoft.Reporting.WinForms.ReportDataSource dataset_cat = new Microsoft.Reporting.WinForms.ReportDataSource("catDS", list_cat); // set the datasource
+            reportViewer1.LocalReport.DataSources.Add(dataset_cat);
+            dataset_cat.Value = list_cat;
+            reportViewer1.LocalReport.Refresh();
+            reportViewer1.RefreshReport(); // refresh report
+
+            ///
+            ///Tabla y Grafica de Categoria de altura(cat)
+            ///
+            List<Cad> list_cad = getCads();  //get list of students
+            //reportViewer1.LocalReport.DataSources.Clear(); //clear report
+            //reportViewer1.LocalReport.ReportEmbeddedResource = "Syldesk.ReporteFormato.rdlc"; // bind reportviewer with .rdlc
+            Microsoft.Reporting.WinForms.ReportDataSource dataset_cad = new Microsoft.Reporting.WinForms.ReportDataSource("cadDS", list_cad); // set the datasource
+            reportViewer1.LocalReport.DataSources.Add(dataset_cad);
+            dataset_cad.Value = list_cad;
+            reportViewer1.LocalReport.Refresh();
+            reportViewer1.RefreshReport(); // refresh report
+
+
+
         }
 
         private void GenerarReporte_Load(object sender, EventArgs e)
         {
-            this.reportViewer1.RefreshReport();            
-
-            List<Cat> list = getCats();  //get list of students
-
-            //reportViewer1.LocalReport.DataSources.Clear(); //clear report
-
-            //reportViewer1.LocalReport.ReportEmbeddedResource = "Syldesk.ReporteFormato.rdlc"; // bind reportviewer with .rdlc
-
-            Microsoft.Reporting.WinForms.ReportDataSource dataset = new Microsoft.Reporting.WinForms.ReportDataSource("catDS", list); // set the datasource
-
-            reportViewer1.LocalReport.DataSources.Add(dataset);
-
-            dataset.Value = list;
-
-            reportViewer1.LocalReport.Refresh();
-
-            reportViewer1.RefreshReport(); // refresh report
+            this.reportViewer1.RefreshReport();                        
         }
 
-        public static List<Cat> getCats()
+        
+        public List<Cat> getCats()
         {
+            List<Cat> list = new List<Cat>();
 
-            List<Cat> list = new List<Cat>
+            string areas_query = getVolumeAreas();
+
+            List<String> null_checker = SqlConnector.anyEspecificValueGet(
+                "SELECT * from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND alturatotal > 0 AND bifurcados = 0",
+                new String[] { "proyecto_id" },
+                new String[] { "" + proyecto.getId() }
+            );
+
+            if (null_checker != null && null_checker.Count > 0)
             {
-                new Cat
+                List<String> aux = SqlConnector.anyEspecificValueGet(
+                    "SELECT Min(alturatotal) as minimo from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND alturatotal > 0 AND bifurcados = 0",
+                    new String[] { "proyecto_id" },
+                    new String[] { "" + proyecto.getId() }
+                );
+
+                double min = Convert.ToDouble(aux[0]);
+
+                aux = SqlConnector.anyEspecificValueGet(
+                    "SELECT Max(alturatotal) as maximo from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND alturatotal > 0 AND bifurcados = 0",
+                    new String[] { "proyecto_id" },
+                    new String[] { "" + proyecto.getId() }
+                );
+
+                double max = Convert.ToDouble(aux[0]);
+                int length = Convert.ToInt32(max / 2.5) - 1;
+
+                double current_rango = 5;
+                double rango_cat = 2.5;
+                double rango_cat2 = rango_cat / 2;
+
+                for (int i = 0; i < length; i++)
                 {
-                    cat = 2,
-                    no_individuos = 102,
-                },
-                new Cat
-                {
-                    cat = 2,
-                    no_individuos = 202,
-                },
-                new Cat
-                {
-                    cat = 3,
-                    no_individuos = 302,
-                },
-            };
+                    String sqlQueryString = "";
+                    double lower_point = current_rango - rango_cat2;
+                    double upper_point = current_rango + rango_cat2;
+                    if (i == 0)
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND alturatotal < " + upper_point;
+                    }
+                    else if (i == (length - 1))
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND alturatotal > " + lower_point;
+                    }
+                    else
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND alturatotal > " + lower_point + " AND alturatotal < " + upper_point;
+                    }
+
+                    aux = SqlConnector.anyEspecificValueGet(
+                        sqlQueryString,
+                        new String[] { "proyecto_id" },
+                        new String[] { "" + proyecto.getId() }
+                    );
+
+
+                    //chart1.Series.Add(new kawaii_lolis.Series("" + current_rango));
+                    //chart1.Series[i].Points.AddXY("" + current_rango, aux[0]);
+                    //chart1.Series[i].ToolTip = "CAT: #VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+                    list.Add(
+                        new Cat
+                        {
+                            cat = current_rango,
+                            no_individuos = Convert.ToInt32(aux[0]),
+                        }
+                    );
+
+                    current_rango += rango_cat;
+                }
+            }
+
             return list;
+        }
+
+        public List<Cad> getCads()
+        {
+            List<Cad> list = new List<Cad>();
+
+            string areas_query = getVolumeAreas();
+
+            List<String> null_checker = SqlConnector.anyEspecificValueGet(
+                "SELECT * from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND diametro > 0 AND bifurcados = 0",
+                new String[] { "proyecto_id" },
+                new String[] { "" + proyecto.getId() }
+            );
+
+            if (null_checker != null && null_checker.Count > 0)
+            {
+                List<String> aux = SqlConnector.anyEspecificValueGet(
+                    "SELECT Min(diametro) as minimo from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND diametro > 0 AND bifurcados = 0",
+                    new String[] { "proyecto_id" },
+                    new String[] { "" + proyecto.getId() }
+                );
+                double min = Convert.ToDouble(aux[0]);
+
+                aux = SqlConnector.anyEspecificValueGet(
+                    "SELECT Max(diametro) as maximo from individuos where proyecto_id = @proyecto_id AND (" + areas_query + ") AND diametro > 0 AND bifurcados = 0",
+                    new String[] { "proyecto_id" },
+                    new String[] { "" + proyecto.getId() }
+                );
+                double max = Convert.ToDouble(aux[0]);
+
+                int length = Convert.ToInt32(max / 5) - 1;
+
+                double current_rango = 10;
+                double rango_cad = 5;
+                double rango_cad2 = rango_cad / 2;
+
+                for (int i = 0; i < length; i++)
+                {
+                    String sqlQueryString = "";
+
+                    double lower_point = current_rango - rango_cad2;
+                    double upper_point = current_rango + rango_cad2;
+                    if (i == 0)
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND diametro < " + upper_point;
+                    }
+                    else if (i == (length - 1))
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND diametro > " + lower_point;
+                    }
+                    else
+                    {
+                        sqlQueryString = "SELECT Count(*) as conteo " +
+                            " from individuos where proyecto_id = @proyecto_id AND bifurcados = 0 AND (" + areas_query + ") AND diametro >= " + lower_point + " AND diametro < " + upper_point;
+                    }
+
+                    aux = SqlConnector.anyEspecificValueGet(
+                        sqlQueryString,
+                        new String[] { "proyecto_id" },
+                        new String[] { "" + proyecto.getId() }
+                    );
+
+                    //chart1.Series.Add(new kawaii_lolis.Series("" + current_rango));
+                    //chart1.Series[i].Points.AddXY("" + current_rango, aux[0]);
+                    //chart1.Series[i].ToolTip = "CaD #VALX\nConteo: #VALY ";          //Tooltips para cada barra
+
+                    list.Add(
+                        new Cad
+                        {
+                            cad = current_rango,
+                            no_individuos = Convert.ToInt32(aux[0]),
+                        }
+                    );
+
+                    //dataGridView1.Rows.Add(current_rango, aux[0]);
+
+                    current_rango += rango_cad;
+                }
+            }
+
+            return list;
+        }
+
+        private string getVolumeAreas()
+        {
+            string areas_query = "";
+            if (proyecto.getArea1Activo() && Convert.ToBoolean(proyecto.getArea1VolCob()))
+            {
+                areas_query += " area = " + proyecto.getArea1Superficie();
+            }
+            if (proyecto.getArea2Activo() && Convert.ToBoolean(proyecto.getArea2VolCob()))
+            {
+                if (areas_query != "")
+                {
+                    areas_query += " OR ";
+                }
+                areas_query += " area = " + proyecto.getArea2Superficie();
+            }
+            if (proyecto.getArea3Activo() && Convert.ToBoolean(proyecto.getArea3VolCob()))
+            {
+                if (areas_query != "")
+                {
+                    areas_query += " OR ";
+                }
+                areas_query += " area = " + proyecto.getArea3Superficie();
+            }
+            if (proyecto.getArea4Activo() && Convert.ToBoolean(proyecto.getArea4VolCob()))
+            {
+                if (areas_query != "")
+                {
+                    areas_query += " OR ";
+                }
+                areas_query += " area = " + proyecto.getArea4Superficie();
+            }
+            return areas_query;
         }
 
         private void ButtonBack_Click(object sender, EventArgs e)
         {
             form1.formRegistro2ToFront(proyecto);
         }
-    }
-
-    /// 
-    ///  DASOMETRICOS
-    /// 
-    public class Cat
-    {
-        public int cat { get; set; }
-        public int no_individuos { get; set; }
-    }
-
-    public class Cad
-    {
-        public int cad { get; set; }
-        public int no_individuos { get; set; }
-    }
-
-    public class No_Individuos
-    {
-        public int no_individuos { get; set; }
-        public int indidviduos_ha { get; set; }
-        public int individuos_scustf { get; set; }
-    }
-
-    public class Area_Basal
-    {
-        public string especie { get; set; }
-        public double suma_ab { get; set; }
-        public double ab_ha { get; set; }
-        public double ab_ha2 { get; set; }
-    }
-
-    public class Volumen
-    {
-        public string especie { get; set; }
-        public double suma_volumen { get; set; }
-        public double volumen_ha { get; set; }
-        public double volumen_ha2 { get; set; }
-    }
-    ///
-    /// IVI
-    ///
-    public class IVI2
-    {
-        public string nombrecientifico { get; set; }
-        public double frec_abs { get; set; }
-        public double frec_rel { get; set; }
-        public double den_abs { get; set; }
-        public double den_rel { get; set; }
-        public double dom_abs { get; set; }
-        public double dom_rel { get; set; }
-        public double ivi { get; set; }
-    }
-    ///
-    /// Diversidad y Riqueza
-    ///
-    public class IDR
-    {
-        public string especie { get; set; }
-        public double ni { get; set; }
-        public double pi { get; set; }
-        public double ln_pi { get; set; }
-        public double shannon { get; set; }
-        public double simpson { get; set; }
     }
 }
---
+++ SylDesk/SylDesk/Objetos.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SylDesk
+{
+    ///
+    ///Objetos de Base de datos
+    ///
+    public class Especie2
+    {
+        public int id { get; set; }
+        public string nombrecientifico { get; set; }
+        public string nombrecomun { get; set; }
+        public string familia { get; set; }
+        public string formadevida { get; set; }
+        public string genero { get; set; }
+        public string categoriadenorma { get; set; }
+    }
+
+    /// 
+    ///  DASOMETRICOS
+    /// 
+    public class Cat
+    {
+        public double cat { get; set; }
+        public int no_individuos { get; set; }
+    }
+
+    public class Cad
+    {
+        public double cad { get; set; }
+        public int no_individuos { get; set; }
+    }
+
+    public class No_Individuos
+    {
+        public int no_individuos { get; set; }
+        public int indidviduos_ha { get; set; }
+        public int individuos_scustf { get; set; }
+    }
+
+    public class Area_Basal
+    {
+        public string especie { get; set; }
+        public double suma_ab { get; set; }
+        public double ab_ha { get; set; }
+        public double ab_ha2 { get; set; }
+    }
+
+    public class Volumen
+    {
+        public string especie { get; set; }
+        public double suma_volumen { get; set; }
+        public double volumen_ha { get; set; }
+        public double volumen_ha2 { get; set; }
+    }
+    ///
+    /// IVI
+    ///
+    public class IVI2
+    {
+        public string nombrecientifico { get; set; }
+        public double frec_abs { get; set; }
+        public double frec_rel { get; set; }
+        public double den_abs { get; set; }
+        public double den_rel { get; set; }
+        public double dom_abs { get; set; }
+        public double dom_rel { get; set; }
+        public double ivi { get; set; }
+    }
+    ///
+    /// Diversidad y Riqueza
+    ///
+    public class IDR
+    {
+        public string especie { get; set; }
+        public double ni { get; set; }
+        public double pi { get; set; }
+        public double ln_pi { get; set; }
+        public double shannon { get; set; }
+        public double simpson { get; set; }
+    }
+
+}
